ebf7ab7a5d8ff9cf0eeae02289086e7d8c3ce26a
AhmadRezaM <AhmadRezaM@WSN-213>
2023-07-27 21:31:17 +0330

    Add project files.

---
+++ Test/Calculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Core;
+
+public class Calculator
+{
+    double _output = 0;
+
+    private void printOnScreen(string msg = "calculated")
+    {
+        Console.WriteLine("Output " + msg + " and final result is " + _output + " .");
+    }
+
+    public void plus(double increaseBy)
+    {
+        _output += increaseBy;
+
+        printOnScreen(" increases by " + increaseBy);
+
+    }
+
+    public void minus(double decreaseBy) {
+        _output -= decreaseBy;
+
+        printOnScreen(" decerases by " + decreaseBy);
+    }
+
+    public void multiply(double multiplyBy) {
+        _output *= multiplyBy;
+
+        printOnScreen(" multiples by " + multiplyBy);
+    }
+
+    public void divide(double divideBy)
+    {
+        _output /= divideBy;
+
+        printOnScreen(" divides by " + divideBy);
+    }
+
+    public void printResult() {
+        printOnScreen();
+    }
+
+    public void Clear() { 
+        _output = 0;
+        Console.WriteLine(" -- Screen was cleared ! ");
+    }
+
+}
---
+++ Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using Calc.Core;
+
+var clc = new Calculator();
+clc.printResult();// 0
+
+clc.plus(13.89); // 13.89
+clc.plus(0.11); // 14
+clc.multiply(2); // 28
+clc.divide(4); // 7
+clc.minus(10.5); // -3.5
+clc.minus(4); // -7.5
+clc.multiply(11);  // -82.5
+clc.minus(3.25); // - 85.75
+clc.multiply(-1); // 85.75
+clc.minus(0.75); // 85
+
+clc.printResult(); // 85
+clc.Clear(
+    );
+clc.printResult(); // 0
